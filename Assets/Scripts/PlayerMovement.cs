ccb953180000b9cb17bfbb32beabe260184ad4bc
ShrekFollower <727210@newcollege.ac.uk>
2023-02-24 07:58:40 +0000

    commit_2
    added dashing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -9,13 +10,24 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     float horizontalMove = 0f;
     private bool sling = false;
+    private bool canDash = true;
+    private bool isDashing = false;
+    private float dashingPower = 20f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr;
 
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         horizontal = Input.GetAxisRaw("Horizontal");
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
@@ -48,6 +60,11 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
 
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
         Flip();
 
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
@@ -72,5 +89,22 @@ public class PlayerMovement : MonoBehaviour
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        speed = dashingPower;
+        tr.emitting= true;
+        yield return new WaitForSeconds(dashingTime);
+        speed = 8f;
+        tr.emitting= false;
+        rb.gravityScale = originalGravity;
+        isDashing= false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash= true;
     }
 }
